68903739806b6d7dd503f6a466eaa283b97503a9
deepakmadan08 <deepakmadan08@github.com>
2020-07-29 02:24:55 +0530

    Adding the Project

---
+++ src/main/java/com/deepak/nagp/webAppliationProject/Application.java
@@ -0,0 +1,9 @@
+package com.deepak.nagp.webAppliationProject;
+
+public class Application {
+	
+	 public static void main(String[] args) {
+	        Operators adder = new Operators();
+	        System.out.println("2 + 2 = " + adder.add(2, 2));
+	    }
+}
---
+++ src/main/java/com/deepak/nagp/webAppliationProject/Operators.java
@@ -0,0 +1,8 @@
+package com.deepak.nagp.webAppliationProject;
+
+public class Operators {
+
+	public int add(int a, int b) {
+        return a + b;
+    }
+}
